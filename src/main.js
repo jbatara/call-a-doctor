5b599e07af04171f3cc7d7511818a56d6fb1caa5
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-09-13 14:36:13 -0700

    WIP Add code to run promise, then do something with results

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
+import $ from 'jQuery';
+import { getConditions, getDoctors } from  './js/promises.js';
+import { printConditionstoDropDown, printDoctors } from './js/htmlGenerator.js';
+
+
 
 $(document).ready(function(){
+  getConditions.then(function(response){
+    debugger;
+    let body = JSON.parse(response);
+    console.log(body);
+    $('span.symptoms').html(printConditionstoDropDown(body));
+  });
+
+  $('form').submit(function(event){
+    event.preventDefault();
+    let location = $('#location').val();
+    let symptom = $('input[name=symptom]').val();
+
+    getDoctors(location,symptom).then(function(response){
+      let body = JSON.parse(response);
+      $('.doctors').html(printDoctors(body));
+    });
+  });
+
+
 
 });
